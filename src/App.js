3f52e8795f8853894d77ca77fa29b6d723308018
steven <steven_ko@paradise-soft.com.tw>
2021-12-13 23:49:13 +0800

    feat: jsx write js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const App = () => {
+  let friends = ['steven', 'mary', 'sean']
   return (
     <div>
-      <h1>My profile</h1>
-      <p>My name is Mike.</p>
-      <p>I am 25 years old.</p>
+      <h1>{ Math.random() * 100 }</h1>
+      <p>My friends are:</p>
+      { friends.map(firend => <p>{ firend }</p>) }
     </div>
   )
 }
