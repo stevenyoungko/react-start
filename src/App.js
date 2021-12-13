1a4f9ea49a059a36fb7366fb7eccf5850935a5dd
steven <steven_ko@paradise-soft.com.tw>
2021-12-13 23:58:21 +0800

    feat: css in jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import './styles/style.css'
 
 const App = () => {
   let friends = ['steven', 'mary', 'sean']
   return (
     <div>
-      <h1>{ Math.random() * 100 }</h1>
+      <h1 style={{ fontSize: '20px', backgroundColor: 'black' }}>{ Math.random() * 100 }</h1>
       <p>My friends are:</p>
-      { friends.map(firend => <p>{ firend }</p>) }
+      { friends.map(firend => (
+        <p>{ firend }</p>) 
+      )}
     </div>
   )
 }
