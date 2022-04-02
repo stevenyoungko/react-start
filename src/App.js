3afdc87905baf4a35cdc87438b60e52c0dc6269c
steven <steven_ko@paradise-soft.com.tw>
2022-04-02 17:39:06 +0800

    feat: add event handling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ const App = () => {
     { name: 'Steven', age: 17, desc: 'Steven is a good guy.' },
     { name: 'Apple', age: 17, desc: 'Apple is a good guy.'}
   ]
+
+  const buttonHandler = e => {
+    e.preventDefault()
+    let date = new Date().toLocaleDateString()
+    alert(date)
+  }
+
+  const buttonHandler2 = msg => {
+    alert(msg)
+  }
+
   return (
     <div>
       <h1 style={{ fontSize: '20px', backgroundColor: 'black' }}>
@@ -16,6 +27,18 @@ const App = () => {
       { friends.map(friend => (
         <Friend name={friend.name} age={friend.age} desc={friend.desc} />
       ))}
+      <hr />
+
+
+      <div>
+        <from action="">
+          <input type="text" />
+          <button onClick={buttonHandler}>Submit</button>
+        </from>
+        
+        {/* 一般button要包 arrow func 不然會先被觸發 */}
+        <button onClick={() => {buttonHandler2('123')}}>Submit2</button>
+      </div>
     </div>
   )
 }
